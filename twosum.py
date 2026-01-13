7a4d1c4d87f54268f9408b68ce090bb8166fee49
Poojasreekalepu <poojasreekalepu@gmail.com>
2026-01-13 10:03:47 +0530

    Add files via upload

---
+++ twosum.py
@@ -0,0 +1,10 @@
+def twoSum(arr, target):
+    seen = {}
+
+    for i in range(len(arr)):
+        need = target - arr[i]
+
+        if need in seen:
+            return [seen[need], i]
+
+        seen[arr[i]] = i
